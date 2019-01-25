4999b0afa7b939fa07a447b79d9f698c0c746ef5
sx <songxuan319@gmail.com>
2019-01-25 13:54:09 +0100

    solve problem : php does not return string to javascript

--- env.js
+++ env.js
@@ -1,35 +1,41 @@
 <?php
+    // set $dir to current folder, where env.js is. This is more flexible.
     $dir = __DIR__;
     $scanResult= scandir($dir);
     $filenames = array_diff($scanResult, array('.', '..', 'bundle.js'));
 ?>
 
-
-
-
 window.env = {
     GIT_COMMIT: '',
-    CLIENT_ID: [''],
+    CLIENT_ID: '',
     AUTH_TOKEN: '',
     PARTNER: '',
-    FILENAMES: ''
+    FILENAMES: '',
+    NAMEARRAY: ['']
 };
 
+// combine all elements in $filenames into a long string. Each homegraph name is separated by comma.
 window.env.FILENAMES = <?php
+
+// declaring a string var named tmp
+$tmp = "";
+
+// returned value is not a string, how to solve this?
 foreach($filenames as $name) {
-    echo( $name . "," );
+    $tmp = $tmp . $name . ",";
 }
+// return $tmp as a string, now it works. PHP return the content of a value to javascript instead of the string itself.
+echo "'" . $tmp . "'";
 ?>
 
-window.env.CLIENT_ID  = window.env.FILENAMES.split(",");
+window.env.NAMEARRAY  = window.env.FILENAMES.split(",");
 
 // What's left is to find all the strings with the substring provided by user.
 // This function should be done in js and in Test.js file (only for now).
 
 
 // outputing 5 strings to test.
-console.info('env: ', window.env.CLIENT_ID[0]);
-console.info('env: ', window.env.CLIENT_ID[1]);
-console.info('env: ', window.env.CLIENT_ID[2]);
-console.info('env: ', window.env.CLIENT_ID[3]);
-console.info('env: ', window.env.CLIENT_ID[4]);
+
+console.info('env: ', window.env.CLIENT_ID);
+console.log('env: ', window.env.NAMEARRAY[2]);
+console.log('env: ', window.env.NAMEARRAY[0]);
